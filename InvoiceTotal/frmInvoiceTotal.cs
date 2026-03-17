e904701dbfdb96004432d30541988838ac7e2b5e
Jacob Joseph <jjoseph8@smartweb.augustatech.edu>
2026-03-16 21:47:18 -0400

    Update Homework 7-1

--- InvoiceTotal/frmInvoiceTotal.cs
+++ InvoiceTotal/frmInvoiceTotal.cs
@@ -9,14 +9,43 @@ namespace InvoiceTotal
 
         private void btnCalculate_Click(object sender, EventArgs e)
         {
-            decimal subtotal = Decimal.Parse(txtSubtotal.Text);
-            decimal discountPct = .25m;
-            decimal discountAmt = Math.Round(subtotal * discountPct, 2);
-            decimal invoiceTotal = Math.Round(subtotal - discountAmt, 2);
+            try
+            {
+                if (txtSubtotal.Text == "")
 
-            txtDiscountPct.Text = discountPct.ToString("p1");
-            txtDiscountAmt.Text = discountAmt.ToString("c");
-            txtTotal.Text = invoiceTotal.ToString("c");
+
+                {
+                    DialogResult dialogResult = MessageBox.Show("Please enter a Subtotal.", "Error");
+                    return;
+                }
+                else
+                { }
+                    decimal subtotal = Decimal.Parse(txtSubtotal.Text);
+
+                if (subtotal <= 0 || subtotal > 10_000)
+                {
+                    {
+                        DialogResult dialogResult = MessageBox.Show("Please enter a valid Subtotal.", "Error");
+                    }
+                }
+
+                else
+                {
+                    decimal discountPct = .25m;
+                    decimal discountAmt = Math.Round(subtotal * discountPct, 2);
+                    decimal invoiceTotal = Math.Round(subtotal - discountAmt, 2);
+
+                    txtDiscountPct.Text = discountPct.ToString("p1");
+                    txtDiscountAmt.Text = discountAmt.ToString("c");
+                    txtTotal.Text = invoiceTotal.ToString("c");
+                }
+                }
+            
+            catch (Exception)
+            {
+                DialogResult dialogResult = MessageBox.Show("Please enter a valid Subtotal.", "Error");
+            }
+            
 
             txtSubtotal.Focus();
         }
